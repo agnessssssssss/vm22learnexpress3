f150ffff69dc41dcdeedc72b557e5838717b0cdd
Agnes Aus <agnes.aus@tptlive.ee>
2024-03-20 13:38:22 +0200

    movie controller

--- src/movieController.js
+++ src/movieController.js
@@ -45,31 +45,50 @@ router.get('/add', (req, res) => {
     res.render('movies/add.njk');
 });
 
- router.post('/add', (req, res) => {
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    movies.movies.push({
-    id: movies.lastId++,
-    name: req.body.movie,
-    year: req.body.year,
-    description: req.body.description,
-    })
-    let json = JSON.stringify(movies);
-  fs.writeFileSync('movies.json', json);
-  res.redirect('/movies/');
- });
+ router.post('/add', async (req, res) => {
+    await Movie.create({
+        name:req.body.movie,
+        year: req.body.year,
+        description: req.body.description });
+    res.redirect('/movies/');
+});
+
+    // let movies = fs.readFileSync('movies.json', 'utf-8');
+    // movies = JSON.parse(movies);
+    // movies.movies.push({
+    // id: movies.lastId++,
+    // name: req.body.movie,
+    // year: req.body.year,
+//     description: req.body.description,
+//     })
+//     let json = JSON.stringify(movies);
+//   fs.writeFileSync('movies.json', json);
+//   res.redirect('/movies/');
+ // });
 
  router.get('/view', (req, res) => {
     let id = parseInt(req.query.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let movie = movies.movies.find( m => m.id === id);
+    let movie = Movie.findOne ({
+        where: {
+            id: req.query.id
+        }
+    });
+    // console.log(movie)
+    // let movies = fs.readFileSync('movies.json', 'utf-8');
+    // movies = JSON.parse(movies);
+    // let movie = movies.movies.find( m => m.id === id);
      //res.render(movies);
     res.render('movies/view.njk', {movie: movie});
 });
 
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', async (req, res) => {
     let id = parseInt(req.params.id);
+    await Movie.update({
+        where: {
+            id: req.params.id
+        }
+    });
+
     let movies = fs.readFileSync('movies.json', 'utf-8');
     movies = JSON.parse(movies);
     let movie = movies.movies.find( m => m.id === id);
@@ -78,6 +97,14 @@ router.get('/edit/:id', (req, res) => {
 
 router.post('/edit/:id', (req, res) => {
     let id = parseInt(req.params.id);
+    await sequelize.query (`UPDATE movies
+    SET name= '${req.body.movie}
+    year= ${req.body.year},
+    description= '${req.body.description}'
+    WHERE id= ${id}; `,
+    {type: QueryTypes.UPDATE});
+    res.redirect('/movies/');
+
     let movies = fs.readFileSync('movies.json', 'utf-8');
     movies = JSON.parse(movies);
     let movie = movies.movies.find( m => m.id === id);
@@ -88,13 +115,19 @@ router.post('/edit/:id', (req, res) => {
 });
 
 router.get('/delete/:id', (req, res) => {
-    let id = parseInt(req.params.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let index = movies.movies.findIndex( m => m.id === id);
-    movies.movies.splice(index, 1)
-     let json = JSON.stringify(movies);
-    fs.writeFileSync('movies.json', json);
+    await Movie.update({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    // let id = parseInt(req.params.id);
+    // let movies = fs.readFileSync('movies.json', 'utf-8');
+    // movies = JSON.parse(movies);
+    // let index = movies.movies.findIndex( m => m.id === id);
+    // movies.movies.splice(index, 1)
+    //  let json = JSON.stringify(movies);
+    // fs.writeFileSync('movies.json', json);
     res.redirect('/movies/');
 });
 
